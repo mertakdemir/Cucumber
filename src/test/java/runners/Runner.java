e4c028876136f86cc572446e627fc387c90d2095
Mert Sefa Akdemir <akdemirmertsafa@gmail.com>
2023-01-24 12:13:13 +0100

    update

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,10 +7,17 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
+        monochrome=true,
         features = "./src/test/resources/features",//PATH OF FEATURES FOLDER
         glue = "stepdefinitions",//PATH OF STEP DEFINITIONS
         dryRun = false,
-        tags = "@parametrizing1"
+        tags = "@data_tables"
 )
 public class Runner {
 
